d795679b5024603c540feb441e395a87b4a21ad4
rumd3x <edmurcardoso@gmail.com>
2018-12-05 01:31:29 -0200

    cs fix
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -1,28 +1,26 @@
 <?php
 require 'vendor/autoload.php';
-
-use Dotenv\Dotenv;
-use Monolog\Logger;
 use CptmAlerts\Modules\Core;
+use Dotenv\Dotenv;
 use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 
 $logger = new Logger('CPTM Alerts');
-$logger->pushHandler(new StreamHandler(__DIR__.'/../Storage/Logs/app.log'));
+$logger->pushHandler(new StreamHandler(__DIR__ . '/../Storage/Logs/app.log'));
 $logger->info('Application initializing.');
 $_ENV['time_start'] = microtime(true);
 try {
-    $dotenv = new Dotenv(__DIR__.'/..');
+    $dotenv = new Dotenv(__DIR__ . '/..');
     $dotenv->load();
     $dotenv->required('SLACK_KEY')->notEmpty();
     $dotenv->required('SLACK_CHANNEL')->notEmpty();
-    $dotenv->required('SLACK_BOT_USERNAME')->notEmpty();
 
     $core = new Core($logger);
     $code = $core->run();
     $logger->info(sprintf("Application executed gracefully! Exit code: %d in %f seconds.", $code, microtime(true) - $_ENV['time_start']));
 } catch (Exception $e) {
     $logger->error(
-        sprintf("Runtime Error in %f seconds", (microtime(true) - $_ENV['time_start'])), 
+        sprintf("Runtime Error in %f seconds", (microtime(true) - $_ENV['time_start'])),
         [
             'Message' => $e->getMessage(),
             'File' => $e->getFile(),
